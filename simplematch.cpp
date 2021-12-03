55a7cd3c7877534cd2803373c5e871aeadd13f1e
oootyman <guilhem.drogue@orange.fr>
2021-12-03 23:57:45 +0100

    SimpleMatch start

--- simplematch.cpp
+++ simplematch.cpp
@@ -2,5 +2,10 @@
 
 simpleMatch::simpleMatch()
 {
-
+    
+    /*-deque<double> currentRandomSequence
+          -deque<int[]> states
+          +match(matrix&, int, int)
+          +initDeques(int)
+          +shift()*/
 }

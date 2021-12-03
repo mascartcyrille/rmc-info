fb64a0ce8d208312051e5cff8366a61de5ddb891
mascartcyrille <mascart.cyrille@gmail.com>
2021-12-03 21:24:50 +0100

    Implemented the output function for matrices

--- main.cpp
+++ main.cpp
@@ -1,26 +1,39 @@
 #include <array>
 #include <random>
+#include <string>
+#include <fstream>
 #include <iostream>
+#include <iterator>
 #include <algorithm>
 
 int const lines = 10;
 int const columns = 10;
-std::array<int, lines*columns> connection_matrix {};
+std::vector<int> connection_matrix(lines * columns, 0);
 
 double const proba = 0.5;
+
+std::string output_file_name {"output_matrix.txt"};
 
 int main() {
   std::cout << "This is the Random Matrix Compression INterference FunctiOn project!" << std::endl;
 
-  std::mt19937_64 prng;
-  std::uniform_real_distribution<double> unif(0., 1.);
+  { // Generate matrix
+    std::mt19937_64 prng;
+    std::uniform_real_distribution<double> unif(0., 1.);
 
-  std::generate(connection_matrix.begin(), connection_matrix.end(), [&](){return (unif(prng)<=proba)? 1: 0;});
+    std::generate(connection_matrix.begin(), connection_matrix.end(), [&](){return (unif(prng)<=proba)? 1: 0;});
 
-  for(int i {}; i < lines; ++i) {
-    for(int j {}; j < lines; ++j) {
-      std::cout << connection_matrix[i*lines + j] << " ";
+    for(int i {}; i < lines; ++i) {
+      for(int j {}; j < lines; ++j) {
+        std::cout << connection_matrix[i*lines + j] << " ";
+      }
     }
+  }
+
+  { // Write matrix
+    std::ofstream ofs(output_file_name);
+    std::copy(connection_matrix.begin(), connection_matrix.end(), std::ostream_iterator<int>(ofs, " "));
+    ofs.close();
   }
 
   return 0;

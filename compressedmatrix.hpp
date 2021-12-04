a4b72e2a83547d5a493e047418dc03ae48401f98
mascartcyrille <mascart.cyrille@gmail.com>
2021-12-04 02:35:28 +0100

    Re-implemented compressedmatrix using rngstate

--- compressedmatrix.hpp
+++ compressedmatrix.hpp
@@ -6,10 +6,24 @@
 
 #include "rng.hpp"
 
-class CompressedMatrix: public std::vector<RNGState>
-{
+class CompressedMatrix: public std::vector<RNGState*> {
 public:
-  CompressedMatrix(): std::vector<RNGState>() {}
+  CompressedMatrix(int sz): std::vector<RNGState*>(sz, nullptr), size(sz), nbr_non_null_elements(0) {}
+
+  void setElement(size_t pos, RNGState* state) {
+    if(state != nullptr) {
+      ++nbr_non_null_elements;
+      (*this)[pos] = state;
+    }
+  }
+
+  bool full() {
+    return nbr_non_null_elements==size;
+  }
+
+private:
+  int size;
+  int nbr_non_null_elements;
 };
 
 #endif // COMPRESSEDMATRIX_HPP
